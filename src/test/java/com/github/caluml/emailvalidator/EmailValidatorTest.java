1577136a1c598bb7143e81afe965f69ab701230a
Calum <calum@home>
2016-10-12 21:11:27 +0100

    Ignore tests that don't currently pass

--- src/test/java/com/github/caluml/emailvalidator/EmailValidatorTest.java
+++ src/test/java/com/github/caluml/emailvalidator/EmailValidatorTest.java
@@ -1,5 +1,6 @@
 package com.github.caluml.emailvalidator;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
@@ -40,5 +41,24 @@ public class EmailValidatorTest {
         for (String emailAddress : emailAddresses) {
             assertTrue(EmailValidator.isValidEmailAddress(emailAddress + "@gmail.com"));
         }
+    }
+
+    @Ignore
+    @Test
+    public void Wikipedia_invalid_examples() {
+        assertFalse("No @ character", EmailValidator.isValidEmailAddress("Abc.example.com"));
+        assertFalse("Only one @ allowed outside quotation marks",
+                EmailValidator.isValidEmailAddress("A@b@c@example.com"));
+        assertFalse("None of the special characters in this local part are allowed outside quotation marks",
+                EmailValidator.isValidEmailAddress("a\"b(c)d,e:f;g<h>i[j\\k]l@example.com"));
+        assertFalse("Quoted strings must be dot separated or the only element making up the local-part",
+                EmailValidator.isValidEmailAddress("just\"not\"right@example.com"));
+        assertFalse("Spaces, quotes, and backslashes may only exist when within quoted strings and preceded by a backslash",
+                EmailValidator.isValidEmailAddress("this is\"not\\allowed@example.com"));
+        assertFalse("Even if escaped (preceded by a backslash), spaces, quotes, and backslashes must still be contained by quotes",
+                EmailValidator.isValidEmailAddress("this\\ still\\\"not\\\\allowed@example.com"));
+        assertFalse("Double dot after @", EmailValidator.isValidEmailAddress("john.doe@example..com"));
+        assertFalse("a valid address with a leading space", EmailValidator.isValidEmailAddress(" foo@gmail.com"));
+        assertFalse("a valid address with a trailing space", EmailValidator.isValidEmailAddress("foo@gmail.com "));
     }
 }
